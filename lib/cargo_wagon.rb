8129ad67243fff84a17dea955ebfc747be939d7f
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-01-27 12:42:00 +0300

    Create wagons classes

---
+++ lib/cargo_wagon.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Cargo wagon
+class CargoWagon
+  attr_reader :type
+
+  def initialize
+    @type = 'cargo'
+  end
+end
---
+++ lib/passenger_wagon.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Passenger Wagon
+class PassengerWagon
+  attr_reader :type
+
+  def initialize
+    @type = 'passenger'
+  end
+end
