4b757c75493c2a1ae9e438787526a957e0fbbad5
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-02-03 07:56:57 +0300

    Add InstanceCounter tests

--- spec/modules/instance_counter_spec.rb
+++ spec/modules/instance_counter_spec.rb
@@ -3,18 +3,51 @@
 require_relative '../../lib/modules/instance_counter'
 require_relative '../../lib/models/station'
 require_relative '../../lib/models/route'
+require_relative '../../lib/models/cargo_train'
+require_relative '../../lib/models/passenger_train'
+require_relative '../../lib/models/train'
 
 describe InstanceCounter do
-  # it 'counts route instances' do
-  #   start = Station.new('Москва')
-  #   finish = Station.new('Магадан')
-  #   Route.new(start, finish)
-  #   Route.new(finish, start)
+  before(:each) do
+    # Иначе считаются инстансы из других тестов
+    Station.instances = nil
+    Route.instances = nil
+    CargoTrain.instances = nil
+    PassengerTrain.instances = nil
+    Train.instances = nil
+  end
 
-  #   expect(Route.instances).to eq(2)
-  # end
+  it 'counts route instances' do
+    start = Station.new('Москва')
+    finish = Station.new('Магадан')
+    Route.new(start, finish)
+    Route.new(finish, start)
 
-  # it 'counts station instances' do
-  #   expect(Station.instances).to eq(2)
-  # end
+    expect(Route.instances).to eq(2)
+  end
+
+  it 'counts station instances' do
+    Station.new('Москва')
+    Station.new('Магадан')
+    expect(Station.instances).to eq(2)
+  end
+
+  context 'trains' do
+    it 'counts cargo trains' do
+      CargoTrain.new('cargo-001')
+
+      expect(CargoTrain.instances).to eq(1)
+    end
+
+    it 'counts passenger trains' do
+      PassengerTrain.new('passenger-001')
+      PassengerTrain.new('passenger-002')
+
+      expect(PassengerTrain.instances).to eq(2)
+    end
+
+    it 'doesn count parent class instances' do
+      expect(Train.instances).to eq(nil)
+    end
+  end
 end
