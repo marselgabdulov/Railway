4f6b0f91d4f3583377b53289d0e482765c3bc0a0
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-02-05 12:58:32 +0300

    Refactor validator test

--- spec/modules/validator_spec.rb
+++ spec/modules/validator_spec.rb
@@ -5,10 +5,10 @@ require_relative '../../lib/models/train'
 
 describe Validator do
   it 'pass validation' do
-    expect(Train.new('001-CT')).to be_truthy
+    expect(Train.new('001-CT').send(:valid?)).to be_truthy
   end
 
   it 'fails validation' do
-    expect { Train.new('001-C') }.to raise_error(RuntimeError, 'Невалидный формат номера')
+    expect { Train.new('001-C').send(:valid?) }.to raise_error(RuntimeError, 'Невалидный формат номера')
   end
 end
