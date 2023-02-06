6e86270f4d91eb6623d444540dbe55d8b13c276c
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-02-06 15:00:17 +0300

    Refactor Wagon, add serial number

--- lib/models/wagon.rb
+++ lib/models/wagon.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+
 require_relative '../modules/producer'
 
 # class Wagon
 class Wagon
-  attr_reader :type
+  attr_reader :type, :serial_number
 
   include Producer
+
+  def initialize
+    @serial_number = SecureRandom.alphanumeric(5)
+  end
 end
